b6580e827d2c9b82347b32d7abfe2bf87b019cf1
Oleg Kozlov (xxblx) <xxblx@posteo.org>
2018-08-02 22:34:37 +0300

    run_server script

--- run_server.py
+++ run_server.py
@@ -1,16 +1,31 @@
+#!VENV/bin/python
 # -*- coding: utf-8 -*-
+
+import argparse
 
 import tornado.httpserver
 import tornado.ioloop
 
 from webapp.app import WebApp
-from webapp.conf import HOST, PORT
 
 
 def main():
-    http_server = tornado.httpserver.HTTPServer(WebApp())
-    http_server.listen(PORT, HOST)
-    tornado.ioloop.IOLoop.current().start()
+    parser = argparse.ArgumentParser(prog='web-hls')
+    parser.add_argument('--db', type=str, required=True,
+                        help='absolute path to db')
+    parser.add_argument('--debug', action='store_true', default=False)
+
+    parser.add_argument('--host', type=str, default='127.0.0.1')
+    parser.add_argument('--port', type=int, default=8888)
+    args = parser.parse_args()
+
+    loop = tornado.ioloop.IOLoop.current()
+    app = WebApp(loop, args.db, args.debug)
+
+    http_server = tornado.httpserver.HTTPServer(app)
+    http_server.listen(args.port, args.host)
+
+    loop.start()
 
 
 if __name__ == '__main__':
